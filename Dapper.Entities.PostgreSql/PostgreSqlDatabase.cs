57293b6401da37d29f26af638a340befe1b89ebe
Adam O'Neil <adamosoftware@gmail.com>
2023-12-29 08:03:39 -0500

    primary constructors

--- Dapper.Entities.PostgreSql/PostgreSqlDatabase.cs
+++ Dapper.Entities.PostgreSql/PostgreSqlDatabase.cs
@@ -5,17 +5,12 @@ using System.Data;
 
 namespace Dapper.Entities.PostgreSql;
 
-public class PostgreSqlDatabase : Database
+public class PostgreSqlDatabase(string connectionString, ILogger<PostgreSqlDatabase> logger, ISqlBuilder sqlBuilder) : Database(logger, sqlBuilder)
 {
-	private readonly string _connectionString;
+	private readonly string _connectionString = connectionString;
 
 	public PostgreSqlDatabase(string connectionString, ILogger<PostgreSqlDatabase> logger) : this(connectionString, logger, new DefaultSqlBuilder())
 	{
-	}
-
-	public PostgreSqlDatabase(string connectionString, ILogger<PostgreSqlDatabase> logger, ISqlBuilder sqlBuilder) : base(logger, sqlBuilder)
-	{
-		_connectionString = connectionString;
 	}
 
 	public override IDbConnection GetConnection() => new NpgsqlConnection(_connectionString);	
--- Dapper.Entities.SqlServer/SqlServerDatabase.cs
+++ Dapper.Entities.SqlServer/SqlServerDatabase.cs
@@ -5,17 +5,12 @@ using System.Data;
 
 namespace Dapper.Entities.SqlServer;
 
-public class SqlServerDatabase : Database
+public class SqlServerDatabase(string connectionString, ILogger<SqlServerDatabase> logger, ISqlBuilder sqlBuilder) : Database(logger, sqlBuilder)
 {
-	private readonly string _connectionString;
+	private readonly string _connectionString = connectionString;
 
 	public SqlServerDatabase(string connectionString, ILogger<SqlServerDatabase> logger) : this(connectionString, logger, new DefaultSqlBuilder())
 	{
-	}
-
-	public SqlServerDatabase(string connectionString, ILogger<SqlServerDatabase> logger, ISqlBuilder sqlBuilder) : base(logger, sqlBuilder)
-	{
-		_connectionString = connectionString;
 	}
 
 	public override IDbConnection GetConnection() => new SqlConnection(_connectionString);
