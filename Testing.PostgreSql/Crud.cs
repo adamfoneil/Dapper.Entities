182c7e46da4ad5194d84ff58b8eeeb7990c983bb
Adam O'Neil <adamosoftware@gmail.com>
2023-12-30 09:06:02 -0500

    postgres save and merge crud

--- Testing.PostgreSql/Crud.cs
+++ Testing.PostgreSql/Crud.cs
@@ -1,4 +1,5 @@
-﻿using Dapper.Entities.Extensions;
+﻿using Dapper;
+using Dapper.Entities.Extensions;
 using Dapper.Entities.PostgreSql;
 using Testing.Data.Entities;
 
@@ -47,5 +48,46 @@ public class Crud : IntegrationBase
 
 		biz = await cn.GetAsync<Business, long>(biz.Id, txn);
 		Assert.IsNull(biz);
+	}
+
+	[TestMethod]
+	public async Task SaveAndMerge()
+	{
+		CrudExtensions.SqlBuilder = new DefaultSqlBuilder();
+
+		using var cn = GetConnection();
+
+		const string KeyValue = "Whatever4";
+
+		await cn.ExecuteAsync("DELETE FROM public.business WHERE user_id = @userId", new { userId = KeyValue });
+
+		var biz = await cn.SaveAsync<Business, long>(new()
+		{
+			DisplayName = "Save and Merge Test",
+			Email = "anyone@nowhere.org",
+			UserId = KeyValue,
+			DateCreated = DateTime.Now
+		});
+
+		var insertedId = biz.Id;
+		Assert.IsTrue(insertedId != 0);
+
+		// perform a merge against the key value we started with, should be the original row
+		var merge = await cn.MergeAsync<Business, long>(new()
+		{
+			UserId = KeyValue,
+			DisplayName = "Save and Merge Test 2",
+			Email = "anyone@nowhere.org"
+		}, onExisting: (newRow, existingRow) =>
+		{
+			newRow.DateModified = DateTime.Now;
+		});
+
+		var mergedId = merge.Id;
+		Assert.IsTrue(mergedId == insertedId);
+
+		var mergedRow = await cn.GetAsync<Business, long>(mergedId) ?? throw new Exception("row not found");
+		Assert.AreEqual(mergedRow.DisplayName, "Save and Merge Test 2");
+		Assert.IsTrue(mergedRow.DateModified.HasValue);
 	}
 }
