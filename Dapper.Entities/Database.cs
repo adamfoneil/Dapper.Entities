d515cad4796042667a32e7dc215285460fb8dc65
Adam O'Neil <adamosoftware@gmail.com>
2024-01-04 06:40:42 -0500

    whitespace fix

--- Dapper.Entities/Database.cs
+++ Dapper.Entities/Database.cs
@@ -33,21 +33,21 @@ public abstract class Database(ILogger<Database> logger, ISqlBuilder sqlBuilder)
 	}
 
 	public async Task<TResult> DoTransactionAsync<TResult>(Func<IDbConnection, IDbTransaction, Task<TResult>> work)
-    {
-        using var cn = GetConnection();
-        if (cn.State != ConnectionState.Open) cn.Open();
+	{
+		using var cn = GetConnection();
+		if (cn.State != ConnectionState.Open) cn.Open();
 
-        using var txn = cn.BeginTransaction();
-        try
-        {
-            var result = await work.Invoke(cn, txn);
-            txn.Commit();
+		using var txn = cn.BeginTransaction();
+		try
+		{
+			var result = await work.Invoke(cn, txn);
+			txn.Commit();
 			return result;
-        }
-        catch
-        {
-            txn.Rollback();
-            throw;
-        }
-    }
+		}
+		catch
+		{
+			txn.Rollback();
+			throw;
+		}
+	}
 }
