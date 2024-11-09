69d1237a70ea3310af3ed95ef00c3d5f6fb20883
Adam O'Neil <adamosoftware@gmail.com>
2024-11-09 14:19:52 -0500

    explicit columns selected

--- Dapper.Entities.SqlServer/DefaultSqlBuilder.cs
+++ Dapper.Entities.SqlServer/DefaultSqlBuilder.cs
@@ -16,7 +16,7 @@ public class DefaultSqlBuilder : SqlBuilder
 
 		return new()
 		{
-			GetById = $"SELECT * FROM {tableName} WHERE [{columnsByPropertyName["Id"].ColumnName}]=@Id",
+			GetById = $"SELECT {SelectColumns(columnMappings)} FROM {tableName} WHERE [{columnsByPropertyName["Id"].ColumnName}]=@Id;",
 			HasAlternateKey = hasAlternateKey,
 			GetByAlternateKey = hasAlternateKey ? BuildGetByAlternateKey(tableName, columnMappings) : string.Empty,
 			Insert = BuildInsert(tableName, columnMappings),
@@ -27,28 +27,30 @@ public class DefaultSqlBuilder : SqlBuilder
 		};
 	}
 
+	private static string SelectColumns(IEnumerable<ColumnMapping> columns) => string.Join(", ", columns.Select(col => $"[{col.ColumnName}]"));
+
 	private static string BuildGetByAlternateKey(string tableName, IEnumerable<ColumnMapping> columns)
 	{
 		var criteria = GetKeyColumnCriteria(tableName, columns, SetExpression);
-		return $"SELECT * FROM {tableName} WHERE {criteria}";
+		return $"SELECT {SelectColumns(columns)} FROM {tableName} WHERE {criteria};";
 	}
 
 	private static string BuildDelete(string tableName, IEnumerable<ColumnMapping> columns)
 	{
 		var deleteCriteria = GetDeleteCriteria(columns, SetExpression);
-		return $"DELETE {tableName} WHERE {deleteCriteria}";
+		return $"DELETE {tableName} WHERE {deleteCriteria};";
 	}
 
 	private static string BuildUpdate(string tableName, IEnumerable<ColumnMapping> columns)
 	{
 		var (setColumns, whereClause) = GetUpdateComponents(columns, SetExpression);
-		return $"UPDATE {tableName} SET {setColumns} WHERE {whereClause}";
+		return $"UPDATE {tableName} SET {setColumns} WHERE {whereClause};";
 	}
 
 	private static string BuildInsert(string tableName, IEnumerable<ColumnMapping> columns)
 	{
 		var (names, values) = GetInsertComponents(columns, (col) => $"[{col.ColumnName}]");
-		return $@"INSERT INTO {tableName} ({names}) VALUES ({values}); SELECT SCOPE_IDENTITY()";
+		return $@"INSERT INTO {tableName} ({names}) OUTPUT [inserted].[Id] VALUES ({values});";
 	}
 
 	private static string SetExpression(ColumnMapping columnMapping) => $"[{columnMapping.ColumnName}]=@{columnMapping.ParameterName}";
--- Testing.SqlServer/SqlBuilder.cs
+++ Testing.SqlServer/SqlBuilder.cs
@@ -14,21 +14,21 @@ public class SqlBuilder
 		var statements = builder.BuildStatements(typeof(SampleEntity));
 
 		Assert.IsTrue(statements.GetById.Equals(
-			"SELECT * FROM [whatever].[Sample] WHERE [Id]=@Id"));
+			"SELECT [Id], [Name], [Description] FROM [whatever].[Sample] WHERE [Id]=@Id;"));
 
 		Assert.IsTrue(statements.Insert.Equals(
-			"INSERT INTO [whatever].[Sample] ([Name], [Description]) VALUES (@Name, @Description); SELECT SCOPE_IDENTITY()"));
+			"INSERT INTO [whatever].[Sample] ([Name], [Description]) OUTPUT [inserted].[Id] VALUES (@Name, @Description);"));
 
 		Assert.IsTrue(statements.Update.Equals(
-			"UPDATE [whatever].[Sample] SET [Name]=@Name, [Description]=@Description WHERE [Id]=@Id"));
+			"UPDATE [whatever].[Sample] SET [Name]=@Name, [Description]=@Description WHERE [Id]=@Id;"));
 
 		Assert.IsTrue(statements.Delete.Equals(
-			"DELETE [whatever].[Sample] WHERE [Id]=@Id"));
+			"DELETE [whatever].[Sample] WHERE [Id]=@Id;"));
 
 		Assert.IsTrue(statements.HasAlternateKey);
 
 		Assert.IsTrue(statements.GetByAlternateKey.Equals(
-			"SELECT * FROM [whatever].[Sample] WHERE [Name]=@Name"));
+			"SELECT [Id], [Name], [Description] FROM [whatever].[Sample] WHERE [Name]=@Name;"));
 	}
 
 	[TestMethod]
@@ -38,16 +38,16 @@ public class SqlBuilder
 		var statements = builder.BuildStatements(typeof(ExoticEntity));
 
 		Assert.IsTrue(statements.GetById.Equals(
-			"SELECT * FROM [dbo].[ExoticEntity] WHERE [ExoticId]=@Id"));
+			"SELECT [ExoticId], [Name], [Value], [Aliased], [DateCreated], [DateModified] FROM [dbo].[ExoticEntity] WHERE [ExoticId]=@Id;"));
 
 		Assert.IsTrue(statements.Insert.Equals(
-			"INSERT INTO [dbo].[ExoticEntity] ([Name], [Value], [Aliased], [DateCreated]) VALUES (@Name, @Value, @AliasedColumn, @DateCreated); SELECT SCOPE_IDENTITY()"));
+			"INSERT INTO [dbo].[ExoticEntity] ([Name], [Value], [Aliased], [DateCreated]) OUTPUT [inserted].[Id] VALUES (@Name, @Value, @AliasedColumn, @DateCreated);"));
 
 		Assert.IsTrue(statements.Update.Equals(
-			"UPDATE [dbo].[ExoticEntity] SET [Name]=@Name, [Value]=@Value, [Aliased]=@AliasedColumn, [DateModified]=@DateModified WHERE [ExoticId]=@Id"));
+			"UPDATE [dbo].[ExoticEntity] SET [Name]=@Name, [Value]=@Value, [Aliased]=@AliasedColumn, [DateModified]=@DateModified WHERE [ExoticId]=@Id;"));
 
 		Assert.IsTrue(statements.Delete.Equals(
-			"DELETE [dbo].[ExoticEntity] WHERE [ExoticId]=@Id"));
+			"DELETE [dbo].[ExoticEntity] WHERE [ExoticId]=@Id;"));
 
 		Assert.IsFalse(statements.HasAlternateKey);
 
@@ -64,18 +64,18 @@ public class SqlBuilder
 			var statements = builder.BuildStatements(type);
 
 			Assert.IsTrue(statements.Insert.Equals(
-				"INSERT INTO [dbo].[CompositeKeyEntity] ([SomethingId], [Name], [Description], [DateCreated]) VALUES (@SomethingId, @Name, @Description, @DateCreated); SELECT SCOPE_IDENTITY()"));
+				"INSERT INTO [dbo].[CompositeKeyEntity] ([SomethingId], [Name], [Description], [DateCreated]) OUTPUT [inserted].[Id] VALUES (@SomethingId, @Name, @Description, @DateCreated);"));
 
 			Assert.IsTrue(statements.Update.Equals(
-				"UPDATE [dbo].[CompositeKeyEntity] SET [SomethingId]=@SomethingId, [Name]=@Name, [Description]=@Description, [DateModified]=@DateModified WHERE [Id]=@Id"));
+				"UPDATE [dbo].[CompositeKeyEntity] SET [SomethingId]=@SomethingId, [Name]=@Name, [Description]=@Description, [DateModified]=@DateModified WHERE [Id]=@Id;"));
 
 			Assert.IsTrue(statements.Delete.Equals(
-				"DELETE [dbo].[CompositeKeyEntity] WHERE [Id]=@Id"));
+				"DELETE [dbo].[CompositeKeyEntity] WHERE [Id]=@Id;"));
 
 			Assert.IsTrue(statements.HasAlternateKey);
 
 			Assert.IsTrue(statements.GetByAlternateKey.Equals(
-				"SELECT * FROM [dbo].[CompositeKeyEntity] WHERE [SomethingId]=@SomethingId AND [Name]=@Name"));
+				"SELECT [Id], [SomethingId], [Name], [Description], [DateCreated], [DateModified] FROM [dbo].[CompositeKeyEntity] WHERE [SomethingId]=@SomethingId AND [Name]=@Name;"));
 		}
 	}
 }
